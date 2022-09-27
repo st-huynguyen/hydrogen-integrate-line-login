a41b4c764651d4623c67e330f06225e297db1929
Huy Nguyen K <huy.nguyen@supremetech.vn>
2022-09-27 18:38:45 +0700

    Init project

---
+++ src/components/LineLogin.client.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import qs from 'qs';
+
+const LineLogin = () => {
+  const [token, setToken] = useState();
+  const [isLoading, setIsLoading] = useState();
+
+  const redirectURI = import.meta.env.PUBLIC_REDIRECT_URL;
+  const clientId = import.meta.env.PUBLIC_CLIENT_ID;
+  const clientSecret = import.meta.env.PUBLIC_CLIENT_SECRET;
+
+  const authorizationParams = {
+    response_type: 'code',
+    client_id: clientId,
+    redirect_uri: redirectURI,
+    state: Math.random().toString(26).slice(2),
+    scope: 'profile openid email',
+  };
+  const searchParams = new URLSearchParams(authorizationParams).toString();
+  const lineAuthoriseURL = `https://access.line.me/oauth2/v2.1/authorize?${searchParams}`;
+
+  useEffect(() => {
+    const returnCode = new URLSearchParams(document.location.search).get(
+      'code'
+    );
+
+    if (returnCode) {
+      const reqUrl = 'https://api.line.me/oauth2/v2.1/token';
+      const reqBody = {
+        grant_type: 'authorization_code',
+        code: returnCode,
+        redirect_uri: redirectURI,
+        client_id: clientId,
+        client_secret: clientSecret,
+      };
+      const reqConfig = {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      };
+
+      setIsLoading(true);
+      axios
+        .post(reqUrl, qs.stringify(reqBody), reqConfig)
+        .then((res) => {
+          console.log(res.data);
+          setToken(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  }, []);
+
+  if (isLoading) {
+    return <h2 className="text-center">Authorizing...</h2>;
+  }
+
+  return (
+    <>
+      {!token ? (
+        <a
+          className="bg-[#06C755] text-white px-2 py-2.5 flex justify-center items-center gap-2 rounded-lg"
+          href={lineAuthoriseURL}
+        >
+          <img className="w-4" src="/line.svg" alt="Line logo" />
+          <span className="font-medium">Login with LINE</span>
+        </a>
+      ) : (
+        <h2 className="text-center">
+          Logined with LINE successfully! <br />
+          See response data in console.
+        </h2>
+      )}
+    </>
+  );
+};
+
+export default LineLogin;
---
+++ src/routes/account/login.server.jsx
@@ -0,0 +1,16 @@
+import React, { Suspense } from 'react';
+import { Layout } from '../../components/Layout.server';
+import LineLogin from '../../components/LineLogin.client';
+
+const Login = () => {
+  return (
+    <Layout>
+      <Suspense></Suspense>
+      <section className="w-full max-w-md mx-auto gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12">
+        <LineLogin />
+      </section>
+    </Layout>
+  );
+};
+
+export default Login;
